822f19afa1df2d7d00944e95a7106f1c7928e63d
taggertt <taggertt@mail.ru>
2023-01-08 15:46:59 +0600

    add test valuate currency. passed.

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -3,19 +3,18 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 
 class MobileBankApiTestV1 {
     @Test
-    void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
+    void shouldReturnDemoAccounts() { // Проверка валидности в значениях валюты
       given()
           .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
       .when()
           .get("/demo/accounts")
-      // Проверки
       .then()
-          .statusCode(200);
+          .statusCode(200)
+              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
